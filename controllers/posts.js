388540f6ce2d34095a7a4d2865e27dc899b47601
Steven Setiawan Kusuma <steven.kusuma@binus.edu>
2022-08-22 09:21:44 +0700

    Updates

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -24,3 +25,16 @@ export const createPost = async (req, res) => {
     }
 }
 
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    try {
+        if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+        const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    
+        res.json(updatedpost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
